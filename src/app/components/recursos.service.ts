b3b7d6c095b07c63d104d04d85a59ab65829a3e2
EquipoX2025 <lulifmolina@gmail.com>
2025-05-22 09:27:24 +0100

    Añadido LocalStorage y orden

--- src/app/components/recursos.service.ts
+++ src/app/components/recursos.service.ts
@@ -7,9 +7,32 @@ import { RecursosInterface } from './recursos-interface';
 export class RecursosService {
   recursosList:RecursosInterface[] = [];
 
+  constructor(){
+    this.cargarLS();
+  }
+
+  cargarLS() {
+    const datos = localStorage.getItem("recursos");
+    if (datos) {
+      this.recursosList = JSON.parse(datos);
+      this.ordenar();
+    }
+  }
+
+  private ordenar() {
+  this.recursosList.sort((a, b) => a.prioridad - b.prioridad);
+  }
+
+  guardarEnLS(){
+    const recursos = this.recursosList;
+    localStorage.setItem("recursos", JSON.stringify(recursos));
+  }
+
   setRecurso(recurso: RecursosInterface){
     if(recurso.nombre !== '' && recurso.categoria !== '' && recurso.prioridad !== 0){
       this.recursosList.push(recurso)
+      this.ordenar();
+      this.guardarEnLS();
     }
   }
 
@@ -19,13 +42,11 @@ export class RecursosService {
 
   deleteRecurso(index: number){
     this.recursosList.splice(index, 1)
+    this.guardarEnLS();
   }
 
   adquirir(index: number){
-    if(!this.recursosList[index].adquirido){
-      this.recursosList[index].adquirido = true;
-    } else {
-      this.recursosList[index].adquirido = false;
-    }
+    this.recursosList[index].adquirido = !this.recursosList[index].adquirido;
+    this.guardarEnLS();
   }
 }
